f68215b0f8a1bde053a09e84320a6df6ede6313e
Madeline LeClair <mpleclai@mtu.edu>
2021-07-20 09:50:24 -0500

    Initial Commit

---
+++ Shared/digital_trainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  digital_trainerApp.swift
+//  Shared
+//
+//  Created by Madeline LeClair on 7/20/21.
+//
+
+import SwiftUI
+
+@main
+struct digital_trainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
